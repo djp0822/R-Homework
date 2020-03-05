7b5f5c6b149326906fba0471f65f8c39a22924af
djp0822 <dimitrios.psaltos@pfizer.com>
2020-03-05 16:01:26 -0500

    Create Machine_learning.R

---
+++ Machine_learning.R
@@ -0,0 +1,132 @@
+library(dslabs)
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+data(reported_heights)
+
+dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
+  filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
+  mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
+  select(sex, type)
+
+y <- factor(dat$sex, c("Female", "Male"))
+x <- dat$type
+
+data.frame(x, y) %>% filter(x == 'online') %>% summary()
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+table(y_dat)
+
+library(caret)
+data(iris)
+iris <- iris[-which(iris$Species=='setosa'),]
+y <- iris$Species
+
+set.seed(2)    # if using R 3.6 or later, use set.seed(2, sample.kind="Rounding")
+test_index <- createDataPartition(y,times=1,p=0.5,list=FALSE)
+test <- iris[test_index,]
+train <- iris[-test_index,]
+
+foo <- function(x){
+  rangedValues <- seq(range(x)[1],range(x)[2],by=0.1)
+  sapply(rangedValues,function(i){
+    y_hat <- ifelse(x>i,'virginica','versicolor')
+    mean(y_hat==train$Species)
+  })
+}
+predictions <- apply(train[,-5],2,foo)
+sapply(predictions,max)	
+
+predictions <- foo(train[,3])
+rangedValues <- seq(range(train[,3])[1],range(train[,3])[2],by=0.1)
+cutoffs <-rangedValues[which(predictions==max(predictions))]
+
+y_hat <- ifelse(test[,3]>cutoffs[1],'virginica','versicolor')
+mean(y_hat==test$Species)
+
+
+set.seed(1)
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90,0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+
+
+set.seed(1) # set.seed(1, sample.kind="Rounding") if using R 3.6 or later
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+dat
+
+rmse <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+})
+
+mean(rmse)
+sd(rmse)
+
+set.seed(1)
+n <- c(100, 500, 1000, 5000, 10000)
+res <- sapply(n, function(n){
+  Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+  dat <- MASS::mvrnorm(n, c(69, 69), Sigma) %>%
+    data.frame() %>% setNames(c("x", "y"))
+  rmse <- replicate(100, {
+    test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+    train_set <- dat %>% slice(-test_index)
+    test_set <- dat %>% slice(test_index)
+    fit <- lm(y ~ x, data = train_set)
+    y_hat <- predict(fit, newdata = test_set)
+    sqrt(mean((y_hat-test_set$y)^2))
+  })
+  c(avg = mean(rmse), sd = sd(rmse))
+})
+
+res
+
+
+set.seed(1)
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.95, 0.95, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+rmse <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+})
+
+mean(rmse)
+sd(rmse)
+
+# KNN
+set.seed(1)
+data("heights")
+library(caret)
+library(dplyr)
+ks <- seq(1, 101, 3)
+F_1 <- sapply(ks, function(k){
+  test_index <- createDataPartition(heights$sex, times = 1, p = 0.5, list = FALSE)
+  test_set <- heights[test_index, ]
+  train_set <- heights[-test_index, ]
+  fit <- knn3(sex ~ height, data = train_set, k = k)
+  y_hat <- predict(fit, test_set, type = "class") %>% 
+    factor(levels = levels(train_set$sex))
+  F_meas(data = y_hat, reference = test_set$sex)
+})
+plot(ks, F_1)
+max(F_1)
+
+
+library(dslabs)
+data("tissue_gene_expression")
+
